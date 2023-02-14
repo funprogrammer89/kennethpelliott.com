c2505bffadd1f22f3de9c8dee0dcd4307083c5d7
Ken Elliott <Kennethpelliott@outlook.com>
2023-02-14 02:54:28 -0500

    update

--- update.py
+++ update.py
@@ -1,6 +1,7 @@
 import os
+val = input("Enter commit text ")
 cmd1 = 'git add .'
-cmd2 = 'git commit -m "l"'
+cmd2 = 'git commit -m "'+val+'"'
 cmd3 = 'git push origin master'
 cmd4 = 'git status'
 os.system(cmd1)
