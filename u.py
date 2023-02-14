333a5d42f452ad5a4dc8464e778662f2f8d99f13
Ken Elliott <Kennethpelliott@outlook.com>
2023-02-14 05:33:33 -0500

    l

--- u.py
+++ u.py
@@ -1,6 +1,6 @@
 import os
 val = input("Enter commit text ")
-print ("----- RUNNING 'git add .' -----")
+print ("f{\033[96m}----- RUNNING 'git add .' -----{\033[0m}")
 os.system('git add .')
 print ("----- RUNNING 'git status' -----")
 os.system('git status')
